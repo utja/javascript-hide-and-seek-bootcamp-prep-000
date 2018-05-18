3a41c3d09a33a628b5a8da29c806648a1ad81d77
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-05-18 01:46:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function deepestChild() {
   var node = document.getElementById('grand-node').querySelectorAll('div');
   var nextNode
   
-  let node = document.getElementById('grand-node')
-  let nextNode = node.children[0]
+  //let node = document.getElementById('grand-node')
+  //let nextNode = node.children[0]
 
   while (nextNode) {
     node = nextNode
