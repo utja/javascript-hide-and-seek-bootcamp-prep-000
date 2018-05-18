61bef260149960fb10af2462777877808fd5a61a
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-05-18 01:47:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,19 +21,10 @@ function deepestChild() {
   var node = document.getElementById('grand-node').querySelectorAll('div');
   var nextNode
   
-  //let node = document.getElementById('grand-node')
-  //let nextNode = node.children[0]
-
-  while (nextNode) {
-    node = nextNode
-    nextNode = node.children[0]
+  for (let i = 0; i < node.length-1; i++) {
+    nextNode = node[i].querySelector('div');
   }
-  return nextNode
-  
- /* for (let i = 0; i < node.length-1; i++) {
-    nextNode = node[i].querySelector('div')
-  }
-  return nextNode */
+  return nextNode;
 }
 
 /*
